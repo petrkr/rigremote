c33fb6a4d763afa30d77bc3ef3b78bbb9bf55ff5
Petr Kracik <petrkr@petrkr.net>
2024-08-25 16:27:50 +0200

    New CSV format, implemented list folder, FIX set_folders
    Signed-off-by: Petr Kracik <petrkr@petrkr.net>

--- transmitter.py
+++ transmitter.py
@@ -2,6 +2,7 @@ import os
 import csv
 import signal
 import sys
+from glob import glob
 import time
 from datetime import datetime, timedelta
 import yaml
@@ -45,22 +46,32 @@ def check_signal_power(rig : Hamlib.Rig, threshold, max_waiting_time):
         time.sleep(10)
     return False
 
-def transmit(rig, set_name, frequency, mode, duration, files, signal_power_threshold, max_waiting_time):
-    log_message(f"Checking signal power before transmission of {files} from {set_name} on {frequency} MHz, Mode: {mode}")
+def transmit(rig : Hamlib.Rig, set_folder, frequency, mode, duration, power, pause, signal_power_threshold, max_waiting_time):
+    log_message(f"Starting transmission of {set_folder} on {frequency} MHz, Power: {power} W")
 
-    if check_signal_power(rig, signal_power_threshold, max_waiting_time):
-        log_message(f"Starting transmission of {files} from {set_name} on {frequency} MHz, Mode: {mode}")
+    rig.set_freq(Hamlib.RIG_VFO_CURR, frequency * 1e6)
+    rig.set_mode(mode)
+    rig.set_level(Hamlib.RIG_LEVEL_RFPOWER, power / 100)
 
-        rig.set_freq(frequency * 1e6)
-        rig.set_mode(mode)
-        
-        for file in files:
-            log_message(f"Transmitting {file}...")
-            time.sleep(duration * 60 / len(files))
-        
-        log_message(f"Finished transmission of {files}")
-    else:
+    log_message(f"Checking signal power before transmission")
+
+    if not check_signal_power(rig, signal_power_threshold, max_waiting_time):
         log_message("Signal power threshold not met. Transmission aborted.", level="error")
+        return
+
+    for file in glob("*.wav", root_dir=set_folder):
+        if not running:
+            log_message(f"Transmission of {set_folder} interrupted by user.")
+            break
+
+        log_message(f"Transmitting {file}...")
+        time.sleep(5)
+
+        log_message(f"Finished transmitting {file}. Waiting for next one")
+        time.sleep(pause)
+
+    log_message(f"Finished transmission of {set_folder}")
+
 
 def check_for_overlaps(schedule):
     events = []
@@ -98,6 +109,7 @@ def parse_mode(mode):
 
 def parse_schedule(file_path):
     schedules = []
+    set_folder = os.path.dirname(file_path)
     try:
         with open(file_path, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -108,7 +120,8 @@ def parse_schedule(file_path):
                 duration_minutes = int(row['Duration (minutes)'])
                 frequency = float(row['Frequency (MHz)'])
                 mode = row['Mode']
-                files = row['Files'].split(';')
+                power = int(row['Power (W)']) or 5
+                pause = int(row['Pause (sec)']) or 60
 
                 start_datetime = datetime.combine(start_date, start_time)
                 end_datetime = start_datetime + timedelta(minutes=duration_minutes)
@@ -120,12 +133,14 @@ def parse_schedule(file_path):
                     end_datetime = start_datetime + timedelta(minutes=duration_minutes)
 
                     schedules.append({
+                        'set_folder': set_folder,
                         'start_datetime': start_datetime,
                         'end_datetime': end_datetime,
                         'duration': duration_minutes,
                         'frequency': frequency,
                         'mode': mode,
-                        'files': files
+                        'power': power,
+                        'pause': pause
                     })
 
                     current_date += timedelta(days=1)
@@ -149,9 +164,20 @@ def check_overlaps(schedules):
                 exit(1)
 
 
-def load_and_check_schedules(file_paths):
+def load_and_check_schedules(transmit_sets_path):
+    schedule_files = []
+    for set_folder in os.listdir(transmit_sets_path):
+        set_path = os.path.join(transmit_sets_path, set_folder)
+        if os.path.isdir(set_path):
+            schedule_file = os.path.join(set_path, 'schedule.csv')
+            if not os.path.exists(schedule_file):
+                log_message(f"Warning: Schedule file not found in set {set_folder}. Skipping.", level="warning")
+                continue
+
+            schedule_files.append(schedule_file)
+
     all_schedules = []
-    for file_path in file_paths:
+    for file_path in schedule_files:
         schedules = parse_schedule(file_path)
         all_schedules.extend(schedules)
 
@@ -173,35 +199,29 @@ def main():
 
     while running:
         now = datetime.now()
-        schedule_files = []
-        schedule = []
-        for set_folder in os.listdir(transmit_sets_path):
-            set_path = os.path.join(transmit_sets_path, set_folder)
-            if os.path.isdir(set_path):
-                schedule_file = os.path.join(set_path, 'schedule.csv')
-                if not os.path.exists(schedule_file):
-                    log_message(f"Warning: Schedule file not found in set {set_folder}. Skipping.", level="warning")
-                    continue
+        schedules = load_and_check_schedules(transmit_sets_path)
 
-                schedule_files.append(schedule_file)
+        print(schedules)
 
-                schedule = load_and_check_schedules(schedule_files)
-                print(schedule)
-
-        for row in schedule:
+        for row in schedules:
+            set_folder = row['set_folder']
             start_time = row['start_datetime'].time()
-            if now.time() >= start_time and now.time() <= (datetime.combine(datetime.today(), start_time) + timedelta(minutes=int(row['Duration (minutes)']))).time():
-                files = row['Files'].split(';')
+            if now.time() >= start_time and now.time() <= (datetime.combine(datetime.today(), start_time) + timedelta(minutes=int(row['duration']))).time():
+                log_message(f"Transmitting set {set_folder} at {now}...")
                 transmit(
                     rig=rig,
-                    set_name=set_folder,
+                    set_folder=set_folder,
                     frequency=float(row['frequency']),
                     mode=parse_mode(row['mode']),
                     duration=int(row['duration']),
-                    files=files,
+                    pause=row['pause'],
+                    power=row['power'],
                     signal_power_threshold=global_settings['signal_power_threshold'],
                     max_waiting_time=global_settings['max_waiting_time']
                 )
+            else:
+                log_message(f"This schedule is not active at the moment. Time: "+ str(row['start_datetime']))
+
         for _ in range(global_settings['check_interval']):
             if not running:
                 break
