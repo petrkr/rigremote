d1d540f5d071f9474086f0cbe673ecd1636a5128
Petr Kracík <petrkr@petrkr.net>
2025-04-24 00:25:38 +0200

    Svxlink: somehow working example

--- svxlink-hamlib-bridge.py
+++ svxlink-hamlib-bridge.py
@@ -6,7 +6,6 @@ import threading
 import time
 import os
 
-import select
 
 class SvxPtyRadio:
     def __init__(self, rig, ptt, sql):
@@ -15,29 +14,46 @@ class SvxPtyRadio:
         self._sql_pty = sql
     
 
-def start_radio_monitor(radio_id, radio, ptt, sql):
+def start_radio_monitor(radio_id, radio, ptt, sql_pty = None):
     prev_state = {}
 
     def monitor():
         nonlocal prev_state
         while True:
-            mode_str, bandwidth = radio.get_mode()
+            if not os.path.exists(sql_pty):
+                time.sleep(1)
+                continue
+            try:
+                with open(sql_pty, "wb") as sql:
+                    while True:
+                        mode_str, bandwidth = radio.get_mode()
 
-            state = {
-                'id': radio_id,
-                'freq': radio.get_freq(),
-                'ptt': radio.get_ptt(),
-                'signal': radio.get_signal_strength(),
-                'mode': mode_str,
-                'bandwidth': bandwidth,
-                'ctcss': radio.get_ctcss_tone(),
-                'dcs': radio.get_dcs_code()
-            }
-            if state != prev_state:
-                print(state)
-                prev_state = state
+                        state = {
+                            'id': radio_id,
+                            'freq': radio.get_freq(),
+                            'ptt': radio.get_ptt(),
+                            'signal': radio.get_signal_strength(),
+                            'mode': mode_str,
+                            'bandwidth': bandwidth,
+                            'ctcss': radio.get_ctcss_tone(),
+                            'dcs': radio.get_dcs_code()
+                        }
+                        if state != prev_state:
+                            print(state)
+                            if state['signal'] > 6:
+                                sql.write(b"O")
+                                sql.flush()
+                            else:
+                                sql.write(b"Z")
+                                sql.flush()
 
-            time.sleep(0.5)
+                            prev_state = state
+
+                        time.sleep(0.5)
+
+            except Exception as e:
+                print(f"[{radio_id}] Error: {e}")
+                time.sleep(1)
     
 
     def check_ptt():
@@ -58,13 +74,16 @@ def start_radio_monitor(radio_id, radio, ptt, sql):
                 print(f"[{radio_id}] Error: {e}")
                 time.sleep(1)
 
-    threading.Thread(target=monitor, daemon=True).start()
-    threading.Thread(target=check_ptt, daemon=True).start()
+    if sql_pty:
+        threading.Thread(target=monitor, daemon=True).start()
+
+    if ptt:
+        threading.Thread(target=check_ptt, daemon=True).start()
 
 
 if __name__ == '__main__':
-    #start_radio_monitor("FT991", HamlibNetRig("127.0.0.1"), "/tmp/svxlink_tx1_ptt", "/tmp/svxlink_tx1_sql")
-    start_radio_monitor("FT991", FakeRadio(), "/tmp/svxlink_tx1_ptt", "/tmp/svxlink_tx1_sql")
+    start_radio_monitor("FT991", HamlibNetRig("127.0.0.1"), None, "/tmp/svxlink_tx1_sql")
+    start_radio_monitor("FT991", FakeRadio(), "/tmp/svxlink_tx1_ptt", None)
     try:
         while True:
             time.sleep(1)
